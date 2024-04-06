f20e25fe4e824c9c095ed7c1081f041452296918
Atsushi <atu729@icloud.com>
2024-04-06 20:04:18 +0900

    delete unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import "./App.css";
-// import "leaflet/dist/leaflet.css";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MapComponent from "./components/maps";
 
 function App() {
-  return (
-    // <div className="App">
-    //   <MapContainer center={[35.6764, 139.65]} zoom={13}>
-    //     <TileLayer
-    //       attribution="&copy; https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}"
-    //       url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-    //     />
-    //     <Marker position={[35.6764, 139.65]}>
-    //       <Popup>基本、東京やから</Popup>
-    //     </Marker>
-    //   </MapContainer>
-    // </div>
-    <MapComponent />
-  );
+  return <MapComponent />;
 }
 
 export default App;
